b125ff384b23ebd7ea97ac9cf9d36913428d9e01
Shashwat Dwivedi <67761122+coolrishi28@users.noreply.github.com>
2025-07-17 03:49:48 +0530

    Update code.py

--- 1. Send WhatsApp Message at Specific Time/code.py
+++ 1. Send WhatsApp Message at Specific Time/code.py
@@ -1,2 +1,6 @@
+#Send a WhatsApp message at 13:30 to a contact
+#Format :- kit.sendwhatmsg('Phone Number', 'Message',Hours, Minutes)
+#It follows 24-hour clock.
+
 import pywhatkit as kit
 kit.sendwhatmsg('+911234567890', 'Hello, this is a scheduled message!', 13, 30)
