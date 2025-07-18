9bb933c7e94f56f6ec0b819dee9afaf149192163
Shashwat Dwivedi <67761122+coolrishi28@users.noreply.github.com>
2025-07-18 15:51:04 +0530

    Update code.py

--- 2. Send an Instant WhatsApp Message (no scheduling)/code.py
+++ 2. Send an Instant WhatsApp Message (no scheduling)/code.py
@@ -1,2 +1,4 @@
+# Sends a message instantly, waits for 15 seconds before sending
+
 import pywhatkit as kit
 kit.sendwhatmsg_instantly('+911234567890', 'Hello instantly!', wait_time = 15, tab_close = True)
