061eaaa91a73fe692dc3b92c3e550dc7aaf6a818
Shashwat Dwivedi <67761122+coolrishi28@users.noreply.github.com>
2025-07-19 05:17:24 +0530

    Update code.py

--- 6. Convert Text to Handwriting (and save as image)/code.py
+++ 6. Convert Text to Handwriting (and save as image)/code.py
@@ -1,2 +1,4 @@
+#It saves a handwritten-styled .png image of the text.
+
 import pywhatkit as kit
-kit.text_to_handwriting("Hello, I am Shashwat!", save_to="handwriting.png")
+kit.text_to_handwriting("Hello, I am Shashwat!", save_to = "handwriting.png")
