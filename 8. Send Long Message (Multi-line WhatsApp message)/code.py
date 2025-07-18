9616d22045e4f924c3f4197c566f4765452bc094
Shashwat Dwivedi <67761122+coolrishi28@users.noreply.github.com>
2025-07-19 05:19:48 +0530

    Create code.py

---
+++ 8. Send Long Message (Multi-line WhatsApp message)/code.py
@@ -0,0 +1,10 @@
+import pywhatkit as kit
+
+long_message = """Hey,
+This is line 1.
+This is line 2.
+This is line 3.
+Regards,
+Shashwat"""
+
+kit.sendwhatmsg('+911234567890', long_message, 17, 20)
