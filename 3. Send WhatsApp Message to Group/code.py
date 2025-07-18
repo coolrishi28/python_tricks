36bf591e879d83b18a01a17072e91e65aa15b6b8
Shashwat Dwivedi <67761122+coolrishi28@users.noreply.github.com>
2025-07-18 16:01:32 +0530

    Update code.py

--- 3. Send WhatsApp Message to Group/code.py
+++ 3. Send WhatsApp Message to Group/code.py
@@ -1,2 +1,5 @@
+# Group ID must be copied from WhatsApp Web's invite link.
+# The first parameter inside the function is the group id which needs to be copied as it is from WhatsApp Web's invite link.
+
 import pywhatkit as kit
 kit.sendwhatmsg_to_group('ABCD1234EFG5678HIJKL', 'Hello group!', 14, 5)
