b81dbd23600fd594dfdf0e2f9022f11a8a546e98
Shashwat Dwivedi <67761122+coolrishi28@users.noreply.github.com>
2025-07-20 05:58:54 +0530

    Update and rename code.py to code.py

--- 7. Send Message at Custom Wait Time (Advanced Control)/code.py
+++ 7. Send Message at Custom Wait Time (Advanced Control)/code.py
@@ -1,2 +1,4 @@
+# Sends the message at 16:10, waits 10 seconds before sending, and closes tab after 3 seconds
+
 import pywhatkit as kit
 kit.sendwhatmsg('+911234567890', 'Timed message with control', 16, 10, wait_time = 10, tab_close = True, close_time = 3)
